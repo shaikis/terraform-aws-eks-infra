d185966a1a77d9994b0fff9a5aef59e57aaa8424
shaikis <shaik.urs.com>
2022-10-27 13:40:54 +0530

    added shell script

--- configure_shared_backend.sh
+++ configure_shared_backend.sh
@@ -3,18 +3,21 @@
 # check terraform software exit or not
 # to run this script we need terraform, awscli and git on the machine.
 
-terraform_state = `terraform version |grep "terraform" |wc -l`
+terraform_state=`terraform version |grep "terraform" |wc -l`
 git_status=`git version  |grep "git version" |wc -l`
-if [[ $terraform_state == 1 ]] && [[ $git_status == 1]]; then
+preset_dir=`pwd`
+if [[ $terraform_state == 1 ]] && [[ $git_status == 1 ]]; then
+   cd $HOME
    git clone https://github.com/shaikis/terraform-aws-shared-state.git
-   check_clone_statu=`ls -ltr ./terraform-aws-shared-state/ |wc -l`
-   if [[ $check_clone_statu == 9 ]]; then
-      cd terraform-aws-shared-state
+   check_clone_status=`ls -ltr $HOME/terraform-aws-shared-state/ |wc -l`
+   if [[ $check_clone_status > 5 ]]; then
+      cd $HOME/terraform-aws-shared-state
       terraform init
       terraform apply -auto-approve
    else
      exit
    fi
+   cd $preset_dir
    terraform init
    terraform apply -auto-approve
 
